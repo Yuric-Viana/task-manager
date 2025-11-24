97abfa48cb6b0ad58ad8e76ffa69137beee9219d
Yuri Viana <ycviana@sga.pucminas.br>
2025-11-24 11:49:02 -0300

    feat: implementar autorização do usuário

---
+++ src/middlewares/verify-user-authorization.ts
@@ -0,0 +1,16 @@
+import { AppError } from "@/utils/AppError"
+import { Request, Response, NextFunction } from "express"
+
+function verifyUserAuthorization(role: string[]) {
+    return (request: Request, response: Response, next: NextFunction) => {
+        if (!request.user) {
+            throw new AppError("Não autorizado.", 401)
+        }
+
+        if (!role.includes(request.user.role)) {
+            throw new AppError("Não autorizado.", 401)
+        }
+
+        return next()
+    }
+}
