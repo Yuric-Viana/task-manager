224193ea50b2ec21b7fb059238a28d05fdc244ce
Yuri Viana <ycviana@sga.pucminas.br>
2025-11-24 10:57:55 -0300

    feat: criar controller e rota para criação de usuário

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
 import express from 'express';
+import "express-async-error"
+
 import { errorHandlingMiddleware } from './middlewares/error-handling';
+import { router } from './routes';
 
 const app = express();
 
 app.use(express.json());
+app.use(router)
 app.use(errorHandlingMiddleware)
 
 export { app };
---
+++ src/controllers/UsersController.ts
@@ -0,0 +1,41 @@
+import { Request, Response } from "express";
+import { AppError } from "@/utils/AppError";
+import { prisma } from "@/lib/prisma";
+import { hash } from "bcrypt";
+import { z } from "zod";
+
+class UsersController {
+    async createUser(request: Request, response: Response) {
+        const bodySchema = z.object({
+            name: z.string().min(3),
+            email: z.string().email(),
+            password: z.string().min(6),
+        })
+
+        const { name, email, password } = bodySchema.parse(request.body);
+
+        const existingUser = await prisma.user.findFirst({
+            where: { email }
+        })
+
+        if (existingUser) {
+            throw new AppError("User with this email already exists", 409);
+        }
+
+        const hashedPassword = await hash(password, 10);
+
+        const user = await prisma.user.create({
+            data: {
+                name, 
+                email,
+                password: hashedPassword,
+            }
+        })
+
+        const { password: _, ...userWithoutPassword } = user;
+
+        return response.json(userWithoutPassword);
+    }
+}
+
+export { UsersController };
