40e65b4a9d30192b26b4d997082643ffbea53b46
Yuri Viana <ycviana@sga.pucminas.br>
2025-11-24 19:42:33 -0300

    feat: adicionar controller para visualização de todos os usuários

--- src/routes/user-routes.ts
+++ src/routes/user-routes.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
 
 import { UsersController } from "@/controllers/UsersController";
+import { ensureAuthenticated } from "@/middlewares/ensure-authenticated";
+import { verifyUserAuthorization } from "@/middlewares/verify-user-authorization";
 
 const userRoutes = Router();
 const usersController = new UsersController();
 
 userRoutes.post("/", usersController.createUser);
+userRoutes.get("/", ensureAuthenticated, verifyUserAuthorization(["admin"]), usersController.getUsers)
 
 export { userRoutes };
