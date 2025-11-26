6623c21ceb44ed8885ad394d1a31d3abc0a97b80
Yuri Viana <ycviana@sga.pucminas.br>
2025-11-26 16:19:11 -0300

    test: adicionar teste para verificar lançamento de exceção na criação de tarefa por membro

--- src/tests/tasks/tasks-controller.test.ts
+++ src/tests/tasks/tasks-controller.test.ts
@@ -16,6 +16,9 @@ describe("TaskController", () => {
     let teamId: string
     let setupUserId: string
     let userToken: string
+    let userTwoId: string
+    let userTwoToken: string
+    let teamTwoId: string
 
     beforeAll(async () => {
         const password = '123456'
@@ -41,6 +44,8 @@ describe("TaskController", () => {
         if(taskId) await prisma.tasks.delete({ where: { id: taskId } })
         if(userId) await prisma.user.delete({ where: { id: userId } })
         if(setupUserId) await prisma.user.delete({ where: { id: setupUserId } })
+        if(teamId) await prisma.teams.delete({ where: { id: teamId } })
+        if(teamTwoId) await prisma.teams.delete({ where: { id: teamTwoId } })
 
         await prisma.$disconnect()
     })
@@ -77,5 +82,42 @@ describe("TaskController", () => {
         expect(taskResponse.status).toBe(201)
         expect(taskResponse.body.title).toBe("Desenvolvimento web")
         expect(taskResponse.body).toHaveProperty("id")
+    })
+
+    it("deve gerar uma exceção", async () => {
+        const user = await prisma.user.create({
+            data: {
+                name: "Test User Two",
+                email: "testusertwo@email.com",
+                password: "123456"
+            }
+        })
+
+        userTwoId = user.id
+
+        userTwoToken = sign({ role: "member" }, JWT_SECRET, {
+            subject: userTwoId,
+            expiresIn: "1d"          
+        })
+
+        const teamResponse = await prisma.teams.create({
+            data: {
+                name: "Equipe front end"
+            }
+        })
+
+        teamTwoId = teamResponse.id
+
+        const response = await request(app).post("/tasks/create-task").set("Authorization", `Bearer ${userTwoToken}`).send({
+            title: "Desenvolvimento web",
+            description: "Desenvolver home page da google em uma semana",
+            status: "pending",
+            priority: "high",
+            assignedTo: userTwoId,
+            teamId: teamResponse.id
+        })
+
+        expect(response.status).toBe(401)
+        expect(response.body.message).toBe("Não autorizado.")
     })
 })
